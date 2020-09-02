e7780ec8b636271be0ed7ae3ff833b797eb7d9f7
Web Development <info@toniramon.es>
2020-09-02 14:43:18 +0200

    added tailwind and configured

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        'src/**/*.js',
+        'src/**/*.jsx',
+        'src/**/*.ts',
+        'src/**/*.tsx',
+        'src/**/*.html',
+    ],
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+}
